a36f0dc2dcac279c45ffacb3124b763cf3c5ba42
Samuelab2007 <90534570+Samuelab2007@users.noreply.github.com>
2023-10-21 18:45:44 -0500

    Avance en la lectura de diccionario y de caracteres

--- codigoLab2.asm
+++ codigoLab2.asm
@@ -1,68 +1,125 @@
 .data
 
 input: .asciiz "digram_test.java"
-output: .asciiz "output.txt"
 dict:	.asciiz "dictionary.txt"
 codedProgram:	.asciiz "output.txt"
 .align 2
-input_buffer:	.space 1024	# Necesita un buffer para poder procesar el texto
+input_buffer:	.space 500	# Necesita un buffer para poder procesar el texto
 .align 2
-dict_buffer:	.space 1024	# To save dictionary.txt data
-output_buffer:	.space 1024
-
+dict_buffer:	.space 420	# To save dictionary.txt data
+.align 2
+output_buffer:	.space 20
+# El output_buffer está enfocado para guardar de a caracter o de a digrama, y se va escribiendo al archivo instantáneamente.
+# Por tanto hay que abrir y escribir constantemente.
 
 .text
+
+	# Abrir y leer archivo de input
 	la $s0, input
 	la $s1, input_buffer
 	jal openToRead	# Read input(digram_test.java)
-	move $a3, $a0
-	# openToRead loads file data onto the Memory
+	jal closeFile	# Cierra el archivo del input
+	# Abre el diccionario y lo lee en el buffer
+	la $s0, dict
+	la $s1, dict_buffer
+	jal openToRead	# Read dictionary
+	jal closeFile # Cierra el diccionario.
 	
+	la $t3, input_buffer	# Direccion inicial del buffer
+	jal processCaracter	# Una vez cargado input, procesa en memoria todos los caracteres de entrada.
 	
-	# FIX: Los buffer se sobreescriben entre ellos en la memoria de programa
-
-	 la $s0, dict
-	 la $s1, dict_buffer
-	 jal openToRead	# Read dictionary
-
-	 jal closeFile	# Close dictionary
-	 move $a0, $a3	# Close input
-	 jal closeFile
-	
-	la $t3, input_buffer	# Address of buffer
-
-	# Idea, usamos un registro para tener en cuenta el offset del registro $t4 a medida que vamos procesando el buffer.
-	# Este registro sería a modo de contador
-
-	jal processCaracter	# Una vez cargado input y dict en la memoria. Se procede a procesar usando ambos buffers.
 
 processCaracter:
+	
+	# Loop que recorre el texto de a par de caracteres
+	# Recorro hasta que encuentre un registro con valor null.     0x00 = null
+	
+	lb $a0, ($t3)	# Caracter izq. de digrama
+	lb $a1, 1($t3)	#Caracter de la derecha del inicial
+	
+	jal searchDict	# Busco en el diccionario el digrama correspondiente.
+	
+	# Escribo en el output el digrama o caracter codificado.
+	jal codeIntoOutput
+	
+	beq $t5, 0x00, end_file	# Fin de archivo.	Debería procesar el último caracter y salirse de la rutina processCaracter
+	
 
-	# Loop que procesa el texto de a par de caracteres
+	addi $t3, $t3, 1	# Avanza de a uno en la memoria del buffer.
 	
-	lb $t4, ($t3)	# Caracter izq. de digrama
-	lb $t5, 1($t3)	#Caracter de la derecha del inicial
+	j processCaracter	# Repite el bucle.
 	
+
+end_file:
 	
-	
-	# Con estos dos se concatenan para formar un digrama, si no se encuentra el digrama en el diccionario
-	# se codifica solo el caracter de la izquierda y se avanza a la siguiente dirección.
-	
-	# Branch en el caso de que encuentra el bigrama
-	
-	# Branch en el caso de que no encuentre el bigrama
-	
-	# Branch para cuando se terminó de leer el documento.
 
 searchDict:
-	# LOOP PARA BUSCAR EL DIGRAMA EN EL DICCIONARIO.
+# Busca el digrama y lo cierra.
+	move $t6, $ra	# Guarda la direccion de retorno.
+	
+	# Busca en el diccionario el digrama o el caracter
+	la $t0, dict_buffer	# Dirección inicial del buffer
+	la $t5, output_buffer	# Guarda la dirección del buffer de escritura
+	jal searchDigram	# Primero buscamos un digrama
+	# $v0 sólo será 2 en el caso de que se haya encontrado un digrama.
+	bne $v0, 2, searchCharacter	# Si no encuentra digrama busca un caracter en el diccionario.
+	
+	
+	jr $t6	# Retorno.
 
 
+searchDigram:	#Loop
 
-# La dirección del archivo se debería cargar como un parametro. Para tener la flexibilidad de leer el archivo java y otras cosas.
-openToRead:	# Abre archivo para leerlo. Se deberían ejecutar una vez en toda la ejecución del programa
+# Entradas: $a0 y $a1 conforman un digrama de la entrada.
+
+# Returns: 2 si encontré un digrama
+# 	   1 si no encontré digrama
+# (también se usa para saber de a cuanto avanzar en el input_buffer)
+	# Cargo digramas del diccionario en registros.
+
+	lb $t1, ($t0)	# Carga dos caracteres contiguos del diccionario.
+	lb $t2, 1($t0)
+
+	beq $t2, 0x00, end_file	# Cuando llego al final, realizo una última busqueda para caracter y continuo el programa.
+
+	beq $a0, $t1, comparaSegundoCaracter	# Si hay coincidencias entre los primeros caracteres, se verifica si coinciden los otros.
+
+	
+	addi $t0, $t0, 1	# Avanzo en el buffer de lectura del diccionario.
+	
+	j searchDigram
+
+comparaSegundoCaracter:
+# TODO: Si encuentro un digrama, paro la busqueda y escribo en output.
+	li $v0, 1
+	beq $a1, $t2, encuentraDigrama	# Si coincide también el segundo caracter del digrama
+	jr $ra
+	
+encuentraDigrama:
+# Returns: $v0: 2 significa que encontró un digrama.
+# 	   TODO: $v1: código a usar para la codificacion. (En este caso es el index a codificar)
+
+	# TODO: Guardo en el buffer el indice que representa el digrama
+	
+	li $v0, 2	# Esto indica que se encontró un digrama, también se podría usar para decidir cuanto avanzar en el diccionario.
+ 
+ 
+ 
+searchCharacter:	# LOOP
+
+	# TODO: Función utilizada para evaluar caracteres individuales. 
+	
+	
+	# Falta todo xd.
+	
+	j searchCharacter
+	#addi $ra, $ra, 4	# para ir a la siguiente instruccion
+	#jr $ra
+
+
+openToRead:	# Abre archivo para leerlo.
 	li $v0, 13	# Abrir archivo
-	move $a0, $s0	# TODO: Reemplazar por registro con la dirección del archivo a usar
+	move $a0, $s0	# Carga la dirección del archivo a leer.
 	li $a1, 0	# Read-only mode.
 	li $a2, 0
 	syscall	# Returns file descriptor
@@ -75,28 +132,33 @@ openToRead:	# Abre archivo para leerlo. Se deberían ejecutar una vez en toda la 
 	
     	jr $ra
     	
-    	# Aquí también debería intentar obtener los datos necesarios para poder ir a procesar los caracteres
 
-openToWrite:
+codeIntoOutput:
+# Abre archivo
 	li $v0, 13
 	la $a0, codedProgram
-	li $a1, 9	# Write-only. Si ya existe lo deja vacío para escribir.
+	li $a1, 9	# Write-only.
 	# Si el flag = 9. Cuando el archivo ya existe. No lo deja vacío sino que escribe concatenando al final de este.
 	li $a2, 0
 	syscall
+	
+	move $a0, $v0	# File descriptor
+# Escribe o concatena al archivo
+	li $v0, 15
+	la $a1, output_buffer
+	li $a2, 2
+	syscall # $v0 returns number of carachters written
+# Cierra el archivo
+	li $v0, 16	# closes file
+	syscall
+	jr $ra	# Returns to caller
 	
 closeFile:
 	# a0 is already setted with file descriptor.
 	li $v0, 16
 	syscall
 	jr $ra
-	
 
-writeFile:
-	move $a0, $t0	# File descriptor
-	li $v0, 15
-	la $a1, output_buffer
-	li $a2, 2
-	syscall # $v0 returns number of carachters written
+	
 	
 	
--- digram_test.java
+++ digram_test.java
@@ -1,1 +1,3 @@
-pruebita
+public class Hola{
+
+}este adrfsdifjosijefois tiene que ser enorme, estoy probando algo por un momentico porfis
